43114ef52cafdb81d8bc114a840bfc7c5a8d29a5
kananthav <kevin.anantha@gmail.com>
2018-10-22 09:15:33 +0800

    Updates

---
+++ precache-manifest.a1eb5b4fdcb59b3238ddbbaacfb53e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62a3eba5e32a1a3da0d8",
+    "url": "/static/js/main.62a3eba5.chunk.js"
+  },
+  {
+    "revision": "9bec308cf7dbfa78e962",
+    "url": "/static/js/1.9bec308c.chunk.js"
+  },
+  {
+    "revision": "62a3eba5e32a1a3da0d8",
+    "url": "/static/css/main.6a81ffc1.chunk.css"
+  },
+  {
+    "revision": "7f3dc7986c7e30658e33b7df0a018f60",
+    "url": "/index.html"
+  }
+];
